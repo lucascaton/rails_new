b13e5be421774f1c29b0f2bfe9c8fe6a6e4f4d2b
Lucas Catón <lucascaton@gmail.com>
2012-04-08 21:44:25 -0300

    Initial template

--- default.rb
+++ default.rb
@@ -1,2 +1,13 @@
-`bundle check`
-# create_file 'test.txt', 'content...'
+module Thor::Actions
+  def source_paths
+    [File.dirname(__FILE__) + '/templates']
+  end
+end
+
+system('bundle check') || puts("\n=> Installing missing gems..."); system('bundle install')
+
+remove_file 'rm public/index.html'
+create_file 'app/assets/images/.gitkeep'
+remove_file 'app/assets/images/rails.png'
+
+copy_file 'Gemfile', 'Gemfile', :force => true
