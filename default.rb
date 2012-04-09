db665240bcab4267681d29e17d8cd279d90367f8
Lucas Catón <lucascaton@gmail.com>
2012-04-08 21:59:20 -0300

    Improve default template

--- default.rb
+++ default.rb
@@ -1,13 +1,22 @@
+# Overwriting the Thor's default source path
 module Thor::Actions
   def source_paths
     [File.dirname(__FILE__) + '/templates']
   end
 end
 
-system('bundle check') || puts("\n=> Installing missing gems..."); system('bundle install')
-
-remove_file 'rm public/index.html'
+# Removing useless files
+remove_file 'public/index.html'
 create_file 'app/assets/images/.gitkeep'
 remove_file 'app/assets/images/rails.png'
 
+# Copying teample files
 copy_file 'Gemfile', 'Gemfile', :force => true
+
+# Gemfile's dependencies verification
+system 'bundle install'
+
+# Git repository initialization
+system 'git init'
+system 'git add .'
+system "git commit -am 'Initial Rails directory structure'"
