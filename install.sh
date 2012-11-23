077b7e7b111e1252dc034610c7057a4d6dcd628b
Lucas Caton <lucascaton@gmail.com>
2012-11-23 19:58:34 -0200

    This repository is now a Ruby Gem :-)

--- install.sh
+++
@@ -1,7 +0,0 @@
-echo
-echo "Starting rails templates installation in ~/.rails_tempaltes..."
-
-git clone git@github.com:lucascaton/rails_templates.git ~/.rails_templates
-echo "alias rails_new='_(){ rails new \$1 --skip-bundle --skip-test-unit -f postgresql -m ~/.rails_templates/default.rb; }; _'" >> ~/.bash_profile
-
-echo "Done!"
