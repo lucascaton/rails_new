2c3c358a84ad1f2775129419f653764735eb302c
Lucas Caton <lucascaton@gmail.com>
2012-12-03 19:53:07 -0200

    Including database.yml template + other adjusts

--- lib/template.rb
+++ lib/template.rb
@@ -1,4 +1,6 @@
 def yes?(question)
+  return true unless @customized
+
   answer = ask "     \e[1m\e[32mpromp".rjust(10) + "  \e[0m#{question} \e[33m(y/n)\e[0m"
   case answer.downcase
   when 'yes', 'y'
@@ -18,7 +20,9 @@ end
 def rewrite_file(file, content)
   system "rm #{file} && > #{file}"
   append_to_file(file, content)
-  system "sed '/^$/N;/^\\n$/D' #{file} > sed_output"
+  # first sed: remove duplicated empty lines
+  # second sed: remove lines with whitespaces
+  system "cat #{file} | sed '/^$/N;/^\\n$/D' | sed '/^ *$/d' > sed_output"
   system "mv sed_output #{file}"
 end
 
@@ -37,14 +41,25 @@ def database_adapter
   end
 end
 
+def database_encoding
+  case database_adapter
+  when 'pg'     then 'unicode'
+  when 'mysql1' then 'utf8'
+  end
+end
 
-@database = prompt 'What database you want to use?', default_answer: 'postgresql'
-@create_commits = yes?('Create first git commits?')
-@static_pages = yes?('Create static pages?')
-@haml = yes?('Use HAML?')
-@pt_br_localees = yes?('Use brazilian portuguese locale (I18n)?')
-# @twitter_bootstrap = yes?('Install and configure Twitter Bootstrap Rails gem?')
-# @devise = yes?('Install and configure Devise gem?')
+
+@app_name   = `pwd`.gsub(/.*\/(.+)/, '\1').strip
+@customized = true
+
+@database            = prompt 'What database you want to use?', default_answer: 'postgresql'
+@customized          = yes? 'Would you like to customize the template options?'
+@create_commits      = yes? 'Create first git commits?'
+# @static_pages        = yes? 'Create static pages?'
+# @haml                = yes? 'Use HAML?'
+@pt_br_localees      = yes? 'Use brazilian portuguese locale (I18n)?'
+# @twitter_bootstrap   = yes? 'Install and configure Twitter Bootstrap Rails gem?'
+# @devise              = yes? 'Install and configure Devise gem?'
 
 
 remove_file 'public/index.html'
@@ -134,6 +149,30 @@ coverage/
 
 # Ignore other files
 rails_best_practices_output.html
+).strip
+
+rewrite_file 'config/database.yml', %Q(
+defaults: &defaults
+  adapter: #{database_adapter}
+  #{database_encoding ? "encoding: #{database_encoding}" : ''}
+  #{database_adapter == 'mysql2' ? 'reconnect: false' : ''}
+  pool: 5
+  #{database_adapter == 'sqlite3' ? 'timeout: 5000' : ''}
+  username:
+  password:
+  #{database_adapter == 'mysql2' ? 'socket: /tmp/mysql.sock' : ''}
+
+development:
+  <<: *defaults
+  #{database_adapter == 'sqlite3' ? 'database: db/development.sqlite3' : "database: #{@app_name}_development"}
+
+test:
+  <<: *defaults
+  #{database_adapter == 'sqlite3' ? 'database: db/test.sqlite3' : "database: #{@app_name}_test"}
+
+production:
+  <<: *defaults
+  #{database_adapter == 'sqlite3' ? 'database: db/production.sqlite3' : "database: #{@app_name}"}
 ).strip
 
 system 'bundle install'
